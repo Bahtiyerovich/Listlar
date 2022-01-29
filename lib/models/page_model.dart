c4f920277bfbd9790d81978c051cf28232e331d5
Kamola1188 <bahtiyarovich88@gmail.com>
2022-01-29 10:51:41 +0500

    dars tugadi

--- lib/models/page_model.dart
+++ lib/models/page_model.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class PageModel {
-  String _name;
-  String _date;
-  Widget _page;
-  String _image;
+  final String _name;
+  final String _date;
+  final Widget _page;
+  final String _image;
 
   PageModel(this._name, this._date, this._page, this._image);
   get name => this._name;
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:homeworks/data/page_list.dart';
+import 'package:homeworks/homework/five/instagram.dart';
 import 'package:homeworks/models/page_model.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,25 +11,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+   PageModel _model = PageModel('Instagram', '_date',  Instagram(), 'data');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.symmetric(vertical: 68),
-        children: 
-          PageData.pages.map((e) => _listTile(e)).toList(),
-      ),
+      body: ListView(padding: EdgeInsets.symmetric(vertical: 68), children: [
+        SizedBox(
+          height: 80,
+          width: MediaQuery.of(context).size.width,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: List.generate(15, (index) => _columnMethod()).toList(),
+          ),
+        ),
+        _listTile(_model),
+        _listTile(_model),
+        _listTile(_model),
+        _listTile(_model),
+        _listTile(_model),
+        _listTile(_model),
+        _listTile(_model),
+        _listTile(_model),
+        _listTile(_model),
+        _listTile(_model),
+        _listTile(_model),
+        _listTile(_model),
+
+
+      ]),
+    );
+  }
+
+  Column _columnMethod() {
+    return Column(
+      children: [
+        CircleAvatar(),
+        Text('data'),
+      ],
     );
   }
 
   Card _listTile(PageModel pageModel) => Card(
-    margin: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
-    child: ListTile(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: ListTile(
         title: Text(pageModel.name),
         subtitle: Text(pageModel.date),
         leading: CircleAvatar(child: Icon(Icons.star)),
         onTap: () {},
-        )
-
-      );
+      ));
 }
